2bd4c27ad2b026587ffc9f2934d0327a059f35b5
David Young <david@code10.ca>
2015-10-07 01:03:05 -0400

    Updating the route.rb file to include routing to accounts, schedule and media.

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   get '/login', to: 'login#index', as: :user_login 
   get '/login/destroy', to: 'login#destroy', as: :user_logout
   match '/dashboard', to: 'dashboard#index', as: :dashboard, via: [:get] 
+  match '/accounts', to: 'accounts#index', as: :accounts, via: [:get] 
+  match '/schedule', to: 'schedule#index', as: :schedule, via: [:get] 
+  match '/media', to: 'media#index', as: :media, via: [:get] 
+
   root 'login#index'
 
 
