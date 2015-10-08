2164e4f95c3ecd246b42b56f493588810a79623f
David Young <david@code10.ca>
2015-10-08 13:13:08 -0400

    Updating routes and nav links

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,22 @@
 Rails.application.routes.draw do
-  post '/login', to: 'login#login'
-  get '/login', to: 'login#index', as: :user_login 
-  get '/login/destroy', to: 'login#destroy', as: :user_logout
-  match '/dashboard', to: 'dashboard#index', as: :dashboard, via: [:get] 
-  match '/accounts', to: 'accounts#index', as: :accounts, via: [:get] 
-  get '/accounts/:id', to: 'accounts#conversation', as: :accounts_conversation
-  get '/accounts/add', to: 'accounts#add', as: :accounts_add
-  match '/schedule', to: 'schedule#index', as: :schedule, via: [:get] 
-  match '/media', to: 'media#index', as: :media, via: [:get] 
-  match '/messages', to: 'messages#index', as: :messages, via: [:get] 
+    post '/login', to: 'login#login'
+    get '/login', to: 'login#index', as: :user_login 
+    get '/login/destroy', to: 'login#destroy', as: :user_logout
+    match '/dashboard', to: 'dashboard#index', as: :dashboard, via: [:get] 
+    match '/accounts', to: 'accounts#index', as: :accounts, via: [:get] 
+    get '/accounts/:id', to: 'accounts#conversation', as: :accounts_conversation
+    get '/accounts/add', to: 'accounts#add', as: :accounts_add
+    match '/schedule', to: 'schedule#index', as: :schedule, via: [:get] 
+    match '/media', to: 'media#index', as: :media, via: [:get] 
+    match '/messages', to: 'messages#index', as: :messages, via: [:get] 
 
-  root 'login#index'
+    match '/company', to: 'company#index', as: :company, via: [:get]
+    match '/users', to: 'users#index', as: :users, via: [:get] 
+    match '/settings', to: 'settings#index', as: :settings, via: [:get] 
+
+    root 'login#index'
 
 
-  match '*path', to: 'errors#routing', via: [:get, :post]
+    match '*path', to: 'errors#routing', via: [:get, :post]
 
 end
