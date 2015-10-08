57ea64ca5b2fe725460f415cf68bf9c3cb3b435e
David <david@code10.ca>
2015-10-08 14:52:30 +0000

    Update route.rb to add accounts conversation route.

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get '/login/destroy', to: 'login#destroy', as: :user_logout
   match '/dashboard', to: 'dashboard#index', as: :dashboard, via: [:get] 
   match '/accounts', to: 'accounts#index', as: :accounts, via: [:get] 
+  get '/accounts/:id', to: 'accounts#conversation', as: 'accounts_conversation'
   match '/schedule', to: 'schedule#index', as: :schedule, via: [:get] 
   match '/media', to: 'media#index', as: :media, via: [:get] 
 
