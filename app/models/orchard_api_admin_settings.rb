14d1349a430462b0ff39363d11686ecd7976781b
David Young <david@code10.ca>
2015-10-11 15:36:43 -0400

    Adding API placeholders.

---
+++ app/models/orchard_api_admin_settings.rb
@@ -0,0 +1,14 @@
+class OrchardApiAdminSettings < ActiveRestClient::Base
+	
+	#api details
+    base_url APP_CONFIG['api_url']   
+    request_body_type :json
+    
+    #enable to true for debugging
+    verbose APP_CONFIG['api_verbose']
+
+    # Request for token
+    post :create, "/token", defaults: {app_key:APP_CONFIG['api_app_key']}
+    
+end
+
