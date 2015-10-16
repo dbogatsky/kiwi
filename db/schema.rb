d0a406a296a15a963e94a183c8a0b3346c210f1f
David <david@code10.ca>
2015-10-16 03:15:39 +0000

    Saving schema.rb file.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151004170111) do
+ActiveRecord::Schema.define(version: 20151015203508) do
+
+  create_table "accounts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "username",   null: false
