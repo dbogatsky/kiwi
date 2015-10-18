b4bdb6e7155c2d741d3476157e157babd6803feb
David <david@code10.ca>
2015-10-18 04:58:36 +0000

    Added the Company model for getting company info and for all actions related to Entities.

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151015203508) do
+ActiveRecord::Schema.define(version: 20151018045437) do
 
   create_table "accounts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "companies", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
