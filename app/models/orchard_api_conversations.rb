42ba017c85e381d93d40a29b611b7b3b933be6f3
David Young <david@code10.ca>
2015-10-18 09:50:18 -0400

    Copied the before_request from orchard_api_user and added to all the orchard api that requires it.

--- app/models/orchard_api_conversations.rb
+++ app/models/orchard_api_conversations.rb
@@ -1,7 +1,10 @@
 class OrchardApiConversations < ActiveRestClient::Base
 	
 	#api details
-    base_url APP_CONFIG['api_url']   
+    base_url APP_CONFIG['api_url']
+    before_request do |name, request|
+        request.headers["Authorization"] = 'Token token="'+$user_token+'", app_key="'+APP_CONFIG['api_app_key']+'"'
+    end    
     request_body_type :json
     perform_caching false
 
--- app/models/orchard_api_logout.rb
+++ app/models/orchard_api_logout.rb
@@ -1,7 +1,10 @@
 class OrchardApiLogout < ActiveRestClient::Base
 	
 	#api details
-    base_url APP_CONFIG['api_url']   
+    base_url APP_CONFIG['api_url']
+    before_request do |name, request|
+    	request.headers["Authorization"] = 'Token token="'+$user_token+'", app_key="'+APP_CONFIG['api_app_key']+'"'
+	end    
     request_body_type :json
     
     #enable to true for debugging
@@ -9,7 +12,7 @@ class OrchardApiLogout < ActiveRestClient::Base
 
     ### Request for logout api ###
     # delete token from system
-    post :remove, "/logout", defaults: {app_key:APP_CONFIG['api_app_key']}
+    post :remove, "/logout"
     
 end
 
