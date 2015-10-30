4f24e36a47048807ee5b62a090fb47e8c0c7620d
David Young <david@code10.ca>
2015-10-30 16:47:02 -0400

    Added the Greeting message depending on time of day.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,24 @@
 class DashboardController < ApplicationController
+
   def index
+
+    # Generate greeting message in the dashboard
+    # to get the current time zone of t is t.zone
+    current_time = Time.current
+    current_time_zone = "UTC" # Stays UTC for now.  Get the time zone from settings later.
+
+    if ( current_time.in_time_zone(current_time_zone).hour < 12 ) # Morning - Before 12:00
+      greeting = 'Good morning '
+    else if ( current_time.in_time_zone(current_time_zone).hour > 11 && current_time.in_time_zone(current_time_zone).hour < 18 ) # Afternoon - Between 11:00 and 18:00
+      greeting = 'Good afternoon '
+    else if ( current_time.in_time_zone(current_time_zone).hour > 17 ) # Evening - After 17:00
+      greeting = 'Good evening '
+    else
+      greeting = 'Hello '
+    end
+
+    @GreetingMessage = greeting + session["user"]["first_name"]
+
   end
+
 end
