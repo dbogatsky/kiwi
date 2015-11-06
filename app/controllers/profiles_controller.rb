dcafb6ec3c44cb21acd4f729f9d432f137f7e3db
GJ <gurpreet@code10.ca>
2015-11-07 00:06:52 +0400

    WIP - fixed up profile page for formatting and fixed image cropper
    form still submits with blank field

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -11,6 +11,6 @@ class ProfilesController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :title, :avatar, :email, :locale, :time_zone)
+    params.require(:user).permit(:first_name, :last_name, :title, :avatar, :locale, :time_zone)
   end
 end
