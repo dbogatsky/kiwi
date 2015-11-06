052aab6d1026f6219be921e2eda51f0d6328f515
David Young <david@code10.ca>
2015-11-06 13:33:29 -0500

    WIP - Saving my placement for getting weather information from Open Weather Map.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -21,4 +21,27 @@ class DashboardController < ApplicationController
 
   end
 
+
+  private
+
+    # More info about the Open Weather Map --> http://openweathermap.org/api
+    # http://api.openweathermap.org/data/2.5/weather?q=Vancouver,CA&APPID=59972cbdeb7c9f8000bcd04dac2aaccd
+    def getWeatherInfo
+      uri = URI.parse("http://api.openweathermap.org/data/2.5/weather")
+
+      # Shortcut
+      #response = Net::HTTP.post_form(uri, {"q" => "Vancouver,CA", "APPID" => APP_CONFIG['open_weather_map']})
+
+      # Full control
+      http = Net::HTTP.new(uri.host, uri.port)
+
+      request = Net::HTTP::Post.new(uri.request_uri)
+      request.set_form_data({"q" => "Vancouver,CA", "APPID" => APP_CONFIG['open_weather_map']})
+
+      response = http.request(request)
+      render :json => response.body
+
+    end
+
+
 end
