dbfc87ab7d2572a59311fcfe31c631e02ff31f28
Zapbuild <w3s.provider@gmail.com>
2016-01-04 17:32:04 +0530

    Remove social likes from popup and add download link

--- app/mailers/user_notifier.rb
+++ app/mailers/user_notifier.rb
@@ -3,7 +3,7 @@ class UserNotifier < ActionMailer::Base
 
   # send a signup email to the user, pass in the user object that   contains the user's email address
   def send_media_email(user)
-	attachments['index.jpeg'] = File.read('app/assets/images/index.jpeg')
+	#attachments['index.jpeg'] = File.read('app/assets/images/index.jpeg')
     mail( :to => user,
     :subject => 'Media File' )
   end
