d4df3c356e5e3427251e6e9edd11bc0e96d44fb7
David Y <david@code10.ca>
2016-02-06 02:37:00 -0500

    Admin: User management not working Saving my work: Added the attributes for the User model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,21 @@
 class User < OrchardApiModel
 
+  schema do
+    # define each attribute separately
+    attribute 'status', :string
+    attribute 'title', :string
+    attribute 'first_name', :string
+    attribute 'last_name', :string
+    #attribute 'username', :string
+    attribute 'email', :string
+    attribute 'company_id', :integer
+    attribute 'company_entity_id', :string
+    attribute 'status_id', :integer
+    attribute 'encrypted_password', :string
+    attribute 'status', :string
+    attribute 'time_zone', :string
+    attribute 'locale', :string
+    attribute 'avatar_url', :string
+  end
+
 end
