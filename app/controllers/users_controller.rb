3e7c53e5e410364f256f35a584f245c7c6aabf45
GJ <gurpreet@code10.ca>
2016-02-12 22:04:47 +0400

    some small trivial changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -52,26 +52,6 @@ class UsersController < ApplicationController
     end
   end
 
-  # def save
-  #   # Save changes from Add/Edit User page
-  #   if params["user-id"].blank?
-  #     # Create new user
-  #     if User.useradd(params["user-email"])
-  #       flash[:success] = 'User has been added successfully'
-  #     else
-  #       flash[:danger] = 'Oops! Unable to add the user'  # Log in error message
-  #     end
-  #   else
-  #     # Edit user
-  #     if User.useredit(params["user-id"], params["user-email"])
-  #       flash[:success] = 'User has been edited successfully'
-  #     else
-  #       flash[:danger] = 'Oops! Unable to edit the user'  # Log in error message
-  #     end
-  #   end
-  #   redirect_to users_path
-  # end
-
 	private
 		def get_token
 		  #set gloal var for token to be used in model, hack for now
