885ce0ff4f1ee7b83a96e77ca9cf50dd1220b40d
GJ <gurpreet@code10.ca>
2016-02-26 20:46:08 +0400

    Spruced up meeting CI and formatting on screen

--- app/helpers/accounts_helper.rb
+++ app/helpers/accounts_helper.rb
@@ -11,4 +11,28 @@ module AccountsHelper
 		end
 		html.html_safe
 	end
+
+
+	def get_styled_meetingstatus(citem, pulled_right=true)
+		pulled_right_class= "pull-right"
+		
+		# status colors
+		statusColor = Hash["scheduled" => "#428BCA", "cancelled" => "#999", "compelted" => "#1CC7C2"]
+
+		if pulled_right == false
+			pulled_right_class = ""
+		end
+
+		if citem.status = "scheduled" or citem.stauts = "cancelled" or citem.stauts = "completed"
+			color = statusColor[citem.status]
+			html = "<span class='badge " + pulled_right_class + "' style='margin-top: -3px; background-color: white; color: #{color}; border: 1px solid #{color};'>#{citem.status}</span>"
+		else citem.status.nil?
+			html = "<span class='badge " + pulled_right_class + "' style='background-color: #ff860b; border-color: #ff860b;'>Unknown</span>"
+		end
+		html.html_safe
+	end
+
+
+
+
 end
