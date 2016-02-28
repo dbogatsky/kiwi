a4e1237c810facafa107c65a9bca4dc7a019365b
David Y <david@code10.ca>
2016-02-28 16:39:54 -0500

    General: Implement Permissions Authorization Updated the ability.rb file to extract permissions from User and Roles and convert them into Can ability

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,6 +2,30 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
+
+    user ||= User.new # guest user (not logged in)
+    roles = Role.all
+
+    # match user's roles with the roles list
+    user.roles.each do | current_user_role |
+      roles.each do | current_role |
+
+        # match found, extract all the permissions and define abilities
+        if current_user_role.id == current_role.id
+          current_role.permissions.each do | permissions |
+
+            #define abilities
+            if permissions.subject_class == 'all' && permissions.action == 'manage'
+              can :manage, :all
+            else 
+
+            end
+          end
+        end
+
+      end
+    end
+
     # Define abilities for the passed in user here. For example:
     #
     #   user ||= User.new # guest user (not logged in)
