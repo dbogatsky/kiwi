e7d3ea8a1246e2346d82a1f01106a22c37c78534
GJ <gurpreet@code10.ca>
2016-03-09 23:15:20 +0400

    hot fix: conversation items

--- app/models/conversation_item_event.rb
+++ app/models/conversation_item_event.rb
@@ -1,5 +1,5 @@
 class ConversationItemEvent < OrchardApiModel
-	  self.site= RequestStore.store[:api_url] + "/conversation_items/:conversation_item_id/"
+  self.prefix = "/conversation_items/:conversation_item_id/"
 	self.element_name = "events"
 	belongs_to :conversation_item
 end
