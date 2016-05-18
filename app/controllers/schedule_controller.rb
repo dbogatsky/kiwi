7f353f067487edeaa0b38acf576eb08e9d8a87aa
GJ <gurpreet@code10.ca>
2016-05-18 15:33:29 -0700

    refactored calendar code to get correct dates and display items correctly
    previously dates and search parameters were not applied

--- app/controllers/schedule_controller.rb
+++ app/controllers/schedule_controller.rb
@@ -61,44 +61,83 @@
     start_date = params['start']
     end_date = params['end']
 
-    search = Hash.new
+    # get all meetings between the date range
+    search = Hash[]
     search[:type_eq] = 'ConversationItems::Meeting'
     search[:starts_at_gteq] = "#{start_date} 00:00:00"
     search[:starts_at_lteq] = "#{end_date} 23:59:59"
 
-    @meetings = ConversationItemSearch.all(params: {search: search, user_ids: user_ids})
+    @meetings = ConversationItemSearch.all(params: { search: search, user_ids: user_ids })
 
-    events = Array.new
+    # get reminders where the date is within the date range
+    search = Hash[]
+    search[:type_eq] = 'ConversationItems::Reminder'
+    search[:scheduled_at_gteq] = "#{start_date} 00:00:00"
+    search[:scheduled_at_lteq] = "#{end_date} 23:59:59"
+
+    @reminders = ConversationItemSearch.all(params: { search: search, user_ids: user_ids })
+
+    # get reminders where the date is within the date range
+    search = Hash[]
+    search[:type_eq] = 'ConversationItems::Quote'
+    search[:ends_at_gteq] = "#{start_date} 00:00:00"
+    search[:ends_at_lteq] = "#{end_date} 23:59:59"
+
+    @quotes = ConversationItemSearch.all(params: { search: search, user_ids: user_ids })
+
+    events = Array[]
+
     @meetings.each do |i|
-      if i.type == 'meeting' || (i.type == 'note' && i.scheduled_at.present?) || i.type == 'reminder' || i.type == 'quote'
-        if i.type == 'meeting'
-          s_date = Chronic.parse(i.starts_at).in_time_zone(current_user.time_zone).strftime('%Y-%m-%dT%H:%M:%S')
-          e_date = Chronic.parse(i.ends_at).in_time_zone(current_user.time_zone).strftime('%Y-%m-%dT%H:%M:%S')
-          color = '#3a87ad'
-          all_day = false
-        elsif i.type == 'note' || i.type == 'reminder'
-          s_date = Chronic.parse(i.scheduled_at).in_time_zone(current_user.time_zone).strftime('%Y-%m-%dT%H:%M:%S')
-          e_date = Chronic.parse(i.scheduled_at).in_time_zone(current_user.time_zone).strftime('%Y-%m-%dT%H:%M:%S')
-          color = '#f0ca45'
-          all_day = false
-        elsif i.type == 'quote'
-          s_date = Chronic.parse(i.ends_at).in_time_zone(current_user.time_zone).strftime('%Y-%m-%dT%H:%M:%S')
-          e_date = Chronic.parse(i.ends_at).in_time_zone(current_user.time_zone).strftime('%Y-%m-%dT%H:%M:%S')
-          color = '#e91e63'
-          all_day = true
-        end
-        event_data = {
-          account_id: i.account_id,
-          id: i.id,
-          title: i.title,
-          start: s_date,
-          end: e_date,
-          color: color,
-          allDay: all_day
-        }
-        events.push(event_data)
-      end
+      s_date = Chronic.parse(i.starts_at).in_time_zone(current_user.time_zone).strftime('%Y-%m-%dT%H:%M:%S')
+      e_date = Chronic.parse(i.ends_at).in_time_zone(current_user.time_zone).strftime('%Y-%m-%dT%H:%M:%S')
+      color = '#3a87ad'
+      all_day = false
+      event_data = {
+        account_id: i.account_id,
+        id: i.id,
+        title: i.title,
+        start: s_date,
+        end: e_date,
+        color: color,
+        allDay: all_day
+      }
+      events.push(event_data)
     end
+
+    @reminders.each do |i|
+      s_date = Chronic.parse(i.scheduled_at).in_time_zone(current_user.time_zone).strftime('%Y-%m-%dT%H:%M:%S')
+      e_date = Chronic.parse(i.scheduled_at).in_time_zone(current_user.time_zone).strftime('%Y-%m-%dT%H:%M:%S')
+      color = '#f0ca45'
+      all_day = false
+      event_data = {
+        account_id: i.account_id,
+        id: i.id,
+        title: i.title,
+        start: s_date,
+        end: e_date,
+        color: color,
+        allDay: all_day
+      }
+      events.push(event_data)
+    end
+
+    @quotes.each do |i|
+      s_date = Chronic.parse(i.ends_at).in_time_zone(current_user.time_zone).strftime('%Y-%m-%dT%H:%M:%S')
+      e_date = Chronic.parse(i.ends_at).in_time_zone(current_user.time_zone).strftime('%Y-%m-%dT%H:%M:%S')
+      color = '#e91e63'
+      all_day = true
+      event_data = {
+        account_id: i.account_id,
+        id: i.id,
+        title: i.title,
+        start: s_date,
+        end: e_date,
+        color: color,
+        allDay: all_day,
+      }
+      events.push(event_data)
+    end
+
     render json: events
   end
 
