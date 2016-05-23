09ff2d2317f8bc246621c9816b48ab495476265e
Shoaib malik <shoaib.malik@ongraph.com>
2016-05-23 15:46:55 +0530

    issue#152 done

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -15,6 +15,12 @@ class NotificationsController < ApplicationController
 	end
 
 	def conversation_detail
-		binding.pry
+		if params[:notification_read] == "false"
+		   @notification = Notification.find(params[:notification_id])
+		   @notification.update_attributes(read_at: Time.now)
+		   notification_info
+		end
+		@notification_type = params[:notification_type]
+		@item = params[:notification_type] == 'account_status_change' ? Account.find(params[:item_id]) : ConversationItem.find(params[:item_id], params: {conversation_id: params[:conversation_id]})
 	end
 end
