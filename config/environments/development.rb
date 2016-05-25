80307d023c4eac9ec884c7764da29b949cc1ee6c
David Y <david@code10.ca>
2016-05-25 00:52:34 -0400

    fix the current_user logout nil error for super admin
    Also removed some old code created by zapbuild

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,19 +40,5 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
-  
-	config.action_view.raise_on_missing_translations = false
-	config.action_mailer.perform_deliveries = true
-	config.action_mailer.default_url_options = { :host => 'http://pgi.zapbuild.com', :port=> '3052' }
-	config.action_mailer.delivery_method = :smtp
-	config.action_mailer.smtp_settings = {
-	address:              'smtp.gmail.com',
-	port:                 587,
-	user_name:            'chandel.annie5@gmail.com',
-	password:             'ztech@44',
-	authentication:       'plain',
-	enable_starttls_auto: true
-	}
-  
 end
 
