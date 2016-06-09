648de8e52767fe0e165af6135251510200ecdcc9
jajjg <gurpreet@code10.ca>
2016-06-09 14:42:24 -0700

    Notification performance improvements (#232)
    * linting fix
    
    * refactored nav bar to reduce calls to orchard
    
    * refactored notification page to reduce calls to orchard and code simplification
    
    * removed debug statement and updated VCR cassette
    
    * use title in notification instead of looking up item

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,26 +1,46 @@
 class NotificationsController < ApplicationController
+  def index
+  end
 
-	def index
-	end
+  def update
+    begin
+      @notification = Notification.find(params[:id])
+      @notification.update_attributes(read_at: Time.now)
+      notification_info
+      @result = true
+    rescue
+      @result = false
+    end
+  end
 
-	def update
-		begin
-		  @notification = Notification.find(params[:id])
-	    @notification.update_attributes(read_at: Time.now)
-	    notification_info
-	    @result = true
-	  rescue
-	  	@result = false
-	  end
-	end
+  def conversation_detail
+    if params[:notification_read] == 'false'
+      @notification = Notification.find(params[:notification_id])
+      @notification.update_attributes(read_at: Time.now)
+      notification_info
+    end
 
-	def conversation_detail
-		if params[:notification_read] == "false"
-		   @notification = Notification.find(params[:notification_id])
-		   @notification.update_attributes(read_at: Time.now)
-		   notification_info
-		end
-		@notification_type = params[:notification_type]
-		@item = params[:notification_type] == 'account_status_change' ? Account.find(params[:item_id]) : ConversationItem.find(params[:item_id], params: {conversation_id: params[:conversation_id]})
-	end
+    user_ids = Array[]
+    user_ids.push(current_user.id)
+
+    @notification_type = params[:notification_type]
+    case params[:notification_type]
+      when 'reminder'
+        search = Hash[]
+        search[:id_eq] = params[:conversation_id]
+        search[:type_eq] = 'ConversationItems::Reminder'
+
+        @item = ConversationItemSearch.all(params: { user_ids: user_ids, search: search })
+      when 'meeting_reminder'
+        search = Hash[]
+        search[:id_eq] = params[:conversation_id]
+        search[:type_eq] = 'ConversationItems::Meeting'
+
+        @item = ConversationItemSearch.all(params: { user_ids: user_ids, search: search })
+      when 'account_status_change'
+        @item = Account.find(params[:conversation_id])
+    end
+    @notification = { id: params[:item_id], type: params[:notification_type] }
+    render template: 'notifications/_conversation_details', layout: false
+  end
 end
