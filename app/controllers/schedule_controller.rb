6c024110541a1bce659e0be06eb502aca9087d40
Shoaib malik <shoaib.malik@ongraph.com>
2016-08-02 17:31:51 +0530

    issue#300_Code refactoring - Reduce API calls - Account (Done)

--- app/controllers/schedule_controller.rb
+++ app/controllers/schedule_controller.rb
@@ -7,7 +7,7 @@ class ScheduleController < ApplicationController
     @user_preference = user_preferences_load
     @users = User.all(uid: session[:user_id])
     get_meetings([current_user.id])
-    @all_account = Account.all.sort_by {|a| a.name.downcase}
+    @all_accounts = Account.all.sort_by {|a| a.name.downcase}
     @sort_meeting = []
     @next_meeting = []
     if @meetings.present?
@@ -225,16 +225,7 @@ class ScheduleController < ApplicationController
   private
 
   def get_meetings(user_ids)
-    # @colors = ['#e0301e', '#000', '#c5e323', '#9e466b', '#0000ff']
     @all_items = []
-    # @user_color = {}
-    # user_ids.each_with_index do |u, index|
-    #   if u.to_i != current_user.id
-    #     @user_color[u.to_i] = @colors[index]
-    #   else
-    #     @user_color[u.to_i] = '#3a87ad'
-    #   end
-    # end
     search = Hash[]
     search[:type_eq] = 'ConversationItems::Meeting'
     search[:item_type_eq] = 'general'
