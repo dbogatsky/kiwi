5cbeeedef7fda046190f4fe02ef13ce5467d23e3
GJ <gurpreet@code10.ca>
2016-08-16 16:05:57 -0700

    removed active resource logging from production

--- config/initializers/activeresource.rb
+++ config/initializers/activeresource.rb
@@ -1,10 +1,10 @@
-if Rails.env.development? or Rails.env.test? or Rails.env.production?
-  active_resource_logger = Logger.new('log/active_resource.log', 'daily'); 
-  active_resource_logger.level = Rails.env.dev? ? Logger::DEBUG : Logger::INFO;
+if Rails.env.development? || Rails.env.test?
+  active_resource_logger = Logger.new('log/active_resource.log', 'daily')
+  active_resource_logger.level = Rails.env.dev? ? Logger::DEBUG : Logger::INFO
   HttpLogger.logger = active_resource_logger # defaults to Rails.logger if Rails is defined
   HttpLogger.colorize = true # Default: true
   HttpLogger.ignore = [/newrelic\.com/]
-  HttpLogger.log_headers = true  # Default: false
+  HttpLogger.log_headers = true # Default: false
   HttpLogger.log_request_body  = true  # Default: true
   HttpLogger.log_response_body = true  # Default: true
   HttpLogger.level = :info # Desired log level as a symbol. Default: :debug
