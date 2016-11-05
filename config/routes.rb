e7b8b9bea8848e67c568aabf330e60784ab81cbc
Shoaib malik <shoaib.malik@ongraph.com>
2016-11-05 15:59:15 +0530

    issue#451_General - Use International Telephone Input JQuery Plugin(Done with validations)

--- config/routes.rb
+++ config/routes.rb
@@ -61,6 +61,7 @@ Rails.application.routes.draw do
       get    :get_users_list # get '/accounts/:id/get_users_list', to: 'accounts#get_users_list'
       get    :load_more_conversation_item
       patch  :update_account_contacts
+      get  :updated_account
     end
     resources :contacts, only: [:edit, :create, :update, :destroy]
     resources :addresses, only: [:edit, :create, :update, :destroy]
