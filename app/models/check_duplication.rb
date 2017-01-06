991a484eabc786d39cbefc2969382a9cd7da83e0
David Y <david@code10.ca>
2017-01-06 01:46:14 -0500

    update the model to handle hash response
    resolve error: "ArgumentError: expected an attributes Hash, got... "

--- app/models/check_duplication.rb
+++ app/models/check_duplication.rb
@@ -1,6 +1,11 @@
 class CheckDuplication < OrchardApiModel
   include ActiveResource::Singleton
-  cached_resource
+  #cached_resource
 
   self.prefix = '/api/v1/accounts/'
+
+  def initialize(parsed = {}, param)
+    @elements = parsed.kind_of?(Hash) ? parsed.values[0] : parsed
+  end
+
 end
