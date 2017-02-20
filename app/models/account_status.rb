c9dce692e9a9e3a92042f854f36626a8a626f1f6
Shoaib malik <shoaib.malik@ongraph.com>
2017-02-20 14:10:39 +0530

    spacing in description

--- app/models/account_status.rb
+++ app/models/account_status.rb
@@ -9,5 +9,5 @@ class AccountStatus < OrchardApiModel
   STATUS = [{name: 'Lead', description: 'Indicates the account is a possible leads', color: '#3071a9'},
             {name: 'Open', description: 'The account is open', color: '#0eb057'},
             {name: 'Suspend', description: 'Suspended account', color: '#f38108'},
-            {name: 'Closed', description:'Closed Account', color: '#999999'}]
+            {name: 'Closed', description: 'Closed Account', color: '#999999'}]
 end
