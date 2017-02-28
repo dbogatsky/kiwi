f850a1d31f1c64c48862dfef0ea272a8c9886615
David Y <david@code10.ca>
2017-02-28 03:52:08 -0500

    add methods to the AccountTransfer model file
    all functions needed to do account transfers and notifications

--- app/models/account_transfer.rb
+++ app/models/account_transfer.rb
@@ -3,23 +3,29 @@ class AccountTransfer < OrchardApiModel
   self.prefix = "/api/v1/"
   self.collection_name = "account_transfers"
 
-
-
-
-# Allow bulk transfers for all the accounts owned by a user POST /account_transfers body:
-# {"account_transfers": [{"from_user_id": "<id>", "to_user_id": "<id>"}]}
-
-#it still supports the old parameters
-# Add endpoint to fetch all transfer that can be approved by the current user GET /api/v1/account_transfers/pending_approval
-
-
-
-#api/v1/accounts/<account_id>/account_transfer/<account_transfer_id>/approve
-#api/v1/accounts/<account_id>/account_transfer/<account_transfer_id>/deny
-
-  def logout
-    ActiveResource::Connection.new(OrchardApiModel.site).delete('/api/v1/logout')
+  # Allow bulk transfers for all the accounts owned by a user POST /account_transfers body:
+  # {"account_transfers": [{"from_user_id": "<id>", "to_user_id": "<id>"}]}
+  #it still supports the old parameters
+  def self.account_transfer_user_to_user(from_user_id, to_user_id)
+    body = "{\"account_transfers\": [{\"from_user_id\": #{from_user_id}, \"to_user_id\": #{to_user_id}}]}"
+    response = ActiveResource::Connection.new(OrchardApiModel.site).post('/api/v1/account_transfers', body, OrchardApiModel.headers)
   end
 
+  # Add endpoint to fetch all transfer that can be approved by the current user GET /api/v1/account_transfers/pending_approval
+  def self.pending_approval
+    response = ActiveResource::Connection.new(OrchardApiModel.site).get('/api/v1/account_transfers/pending_approval', OrchardApiModel.headers)
+  end
+
+  #api/v1/accounts/<account_id>/account_transfer/<account_transfer_id>/approve
+  def self.approve(account_id, account_transfer_id)
+    url = "/api/v1/accounts/#{account_id}/account_transfer/#{account_transfer_id}/approve"
+    response = ActiveResource::Connection.new(OrchardApiModel.site).put(url, '', OrchardApiModel.headers)
+  end
+
+  #api/v1/accounts/<account_id>/account_transfer/<account_transfer_id>/deny
+  def self.deny(account_id, account_transfer_id)
+    url = "/api/v1/accounts/#{account_id}/account_transfer/#{account_transfer_id}/deny"
+    response = ActiveResource::Connection.new(OrchardApiModel.site).put(url, '', OrchardApiModel.headers)
+  end
 
 end
