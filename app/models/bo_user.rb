52a707d2919d6f4b27b2c0f50b28d565c0d63a83
David Y <david@code10.ca>
2017-04-19 05:11:04 -0400

    add the method in the back office user model to do the impersonate api call
    this is for getting the token of the specified user

--- app/models/bo_user.rb
+++ app/models/bo_user.rb
@@ -1,4 +1,10 @@
 class BoUser < OrchardBoApiModel
   self.site = "#{ENV.fetch("ORCHARD_BO_API_HOST")}/companies/:company_id/"
   self.element_name = "users"
+
+  # /backoffice/companies/:company_id/users/:id/impersonate
+  def self.impersonate(company_id, user_id)
+    url = "/backoffice/companies/#{company_id}/users/#{user_id}/impersonate"
+    response = ActiveResource::Connection.new(OrchardBoApiModel.site).post(url, '', OrchardBoApiModel.headers)
+  end
 end
