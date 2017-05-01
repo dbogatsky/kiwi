b559579387b94e61af729fc70c923de9723c0a21
David Y <david@code10.ca>
2017-05-01 05:13:56 -0400

    replace document icons and add extra methods in the medium model to help identify the type of the document

--- app/models/medium.rb
+++ app/models/medium.rb
@@ -14,4 +14,21 @@ class Medium < OrchardApiModel
   def video?
     type.eql?('video')
   end
+
+  def doc_word?
+    payload_content_type.include?('msword') || payload_content_type.include?('wordprocessingml')
+  end
+
+  def doc_excel?
+    payload_content_type.include?('spreadsheetml')
+  end
+
+  def doc_ppt?
+    payload_content_type.include?('presentationml')
+  end
+
+  def doc_pdf?
+    payload_content_type.include?('pdf')
+  end
+
 end
