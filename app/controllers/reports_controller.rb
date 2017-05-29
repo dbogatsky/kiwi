084d2e5e28f00bef4cd79b590843ad403c33afb9
Akhtar ali <akhtarali.upwork@ongraph.com>
2017-05-29 18:38:33 +0530

    Plot a path/route on the google map

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -14,9 +14,18 @@ class ReportsController < ApplicationController
 
   def user_visits
     @user = User.find(params[:user_id])
-    # @lat = @user.addresses.first.latitude
-    # @long = @user.addresses.first.longitude
-    # render json: @user
+    latlng = []
+    if !@user.addresses[0].blank?
+      full_address = (
+                  @user.addresses[0].street_address + ', ' +
+                  @user.addresses[0].city + ', ' +
+                  @user.addresses[0].region + ', ' +
+                  @user.addresses[0].country
+                )
+      latlng = Geocoder.coordinates(full_address)
+    end
+    @latitude = latlng.present? ? latlng[0] : 36.7783
+    @longitude = latlng.present? ? latlng[1] : -119.4179
   end
 
   def meeting_report_result
